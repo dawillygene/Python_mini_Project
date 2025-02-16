5c241f8f5cdedf9c90f374ac8ade5c5d6195df8a
dawillygene <eliamariki2000@gmail.com>
2025-02-16 12:49:43 +0300

    developemnt everyday

--- Short Programs/Matrix Digital Rain.py
+++ Short Programs/Matrix Digital Rain.py
@@ -1,5 +1,11 @@
-import os, time
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("".join([str(randint(0,1)) for _ in range(50)]))
-    time.sleep(0.1)
+import turtle
+
+t = turtle.Turtle()
+t.speed(10)
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+for x in range(200):
+    t.pencolor(colors[x % 6])
+    t.width(x / 100 + 1)
+    t.forward(x)
+    t.left(59)
+turtle.done()
